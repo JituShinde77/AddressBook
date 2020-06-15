e711e49ac55692f4055c07fb8cfa9494ef4d8b16
JituShinde77 <shindejitu77@gmail.com>
2020-06-15 15:03:41 +0530

    Added: Person Detail class.

--- Person.java
+++ Person.java
@@ -1,5 +1,28 @@
 class Person{
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String state;
+	private long zipCode;
+	private long phoneNumber;
+
+	public Person(String firstName, String lastName, String address, String state, long zipCode, long phoneNumber){
+		this.firstName = firstName;
+        	this.lastName = lastName;
+        	this.address = address;
+        	this.state = state;
+        	this.zipCode = zipCode;
+        	this.phoneNumber = phoneNumber;
+	}
+
+	@Override
+	public String toString(){
+		return "Name: "+firstName;
+	}
+
 	public static void main(String[] args){
 		System.out.println("Welcome to Address Book...");
+		Person person = new Person("Jit", "Shinde", "Baramati", "Maharastra", 412103L, 8830763172L);
+		System.out.println(person);
 	}
 }
